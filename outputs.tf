52e3363b18b4ce6f7f3854da1bbad35ec608c86e
mosksky <mosksky@gmail.com>
2017-01-21 11:18:12 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "server_address" {
     value = "${aws_instance.server.0.public_dns}"
 }
+
+output "LV ami" { 
+    value = "${lookup(var.ami, "${var.region}-${var.platform}")}"
+ }
+ 
+ output "LV region - platform" {
+    value = "${var.region}-${var.platform}"
+ }
+ 
+ output "LV count" {
+    value = "${var.servers}"
+ }
+ 
+ output "LV SG" {
+    value = "${aws_security_group.consul.name}"
+ }
